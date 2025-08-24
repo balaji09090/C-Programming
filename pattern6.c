181156401135c7019162dfdde51b5c9e8464b8e8
Balaji Jadhav <balaji.jadhav.0900@gmail.com>
2025-08-24 19:21:55 +0530

    Add files via upload

---
+++ pattern6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, n;
+    printf("Enter A Number :");
+    scanf("%d",&n);
+    for (i = n; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        
+            printf("* ");
+        
+        printf("\n");
+    }
+    return 0;
+}
